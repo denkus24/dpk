9a4b6bca035cdecd3931eba39ac5470c19621157
denkus <62442804+denkus24@users.noreply.github.com>
2022-05-12 10:04:02 +0300

    Add files via upload

--- dpk/recentFilesReader.py
+++ dpk/recentFilesReader.py
@@ -1,14 +1,24 @@
-import codecs
+import codecs, os.path
 
+def rewriteConfigFile(files):
+    with codecs.open('config/recentFiles.cfg', 'w', 'utf-8') as file:
+        writeText = '\n'.join(files)
+        file.write(writeText)
 
 def readAllFiles():
     with codecs.open('config/recentFiles.cfg', 'r', 'utf-8') as file:
         recentFiles = file.read().split('\n')
     if recentFiles[0] == '' and len(recentFiles) == 1:
         recentFiles = None
+        return recentFiles
     else:
         if recentFiles[0] == '':
             del(recentFiles[0])
+    for index,recentFile in enumerate(recentFiles):
+        if not os.path.exists(recentFile):
+            del(recentFiles[index])
+            rewriteConfigFile(recentFiles)
+
 
     return recentFiles
 
